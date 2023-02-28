731ece4a07f2f21cc726933809cd6e2af3c10b2c
EC2 Default User <ec2-user@ip-172-31-83-81.ec2.internal>
2023-02-28 08:50:55 +0000

    setting admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
+  get '/about' => 'homes#about'
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.create!(
+    name: "管理人",
+    email: "kadokanri@outlook.jp",
+    password: "kadokanri_sumer",
+ )
